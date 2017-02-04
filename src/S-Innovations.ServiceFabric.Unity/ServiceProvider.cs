432562d97f54ced1e39d9fe67ff5cb2b3a7c5107
Poul Kjeldager Sørensen <pks@s-innovations.net>
2017-02-05 00:01:19 +0100

    updated nugets

--- src/S-Innovations.ServiceFabric.Unity/ServiceProvider.cs
+++ src/S-Innovations.ServiceFabric.Unity/ServiceProvider.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.Practices.Unity;
 
 namespace SInnovations.ServiceFabric.Unity
