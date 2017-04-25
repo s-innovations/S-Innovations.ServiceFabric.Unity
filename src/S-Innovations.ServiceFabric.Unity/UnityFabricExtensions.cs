5982534527e1dac033ad397eecd2162385c0e2e9
Poul Kjeldager Sørensen <pks@s-innovations.net>
2017-04-25 16:26:34 +0200

    using aspnet core 2.0 instead to get latest feature

--- src/S-Innovations.ServiceFabric.Unity/UnityFabricExtensions.cs
+++ src/S-Innovations.ServiceFabric.Unity/UnityFabricExtensions.cs
@@ -11,7 +11,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.Extensions.PlatformAbstractions;
 using Microsoft.Practices.Unity;
 using Microsoft.ServiceFabric.Actors;
 using Microsoft.ServiceFabric.Actors.Runtime;
