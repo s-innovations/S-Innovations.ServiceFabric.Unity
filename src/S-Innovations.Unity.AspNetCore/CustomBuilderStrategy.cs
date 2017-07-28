54a2709f74c2be2dd4586eb2d8ac0169e5cd75de
Poul Kjeldager Sørensen <pks@s-innovations.net>
2017-07-28 18:47:20 +0200

    default policy throws when 2 ctors of equal length are found

--- src/S-Innovations.Unity.AspNetCore/CustomBuilderStrategy.cs
+++ src/S-Innovations.Unity.AspNetCore/CustomBuilderStrategy.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Practices.ObjectBuilder2;
 using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.ObjectBuilder;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -41,34 +42,74 @@ namespace SInnovations.Unity.AspNetCore
 
             public SelectedConstructor SelectConstructor(IBuilderContext context, IPolicyList resolverPolicyDestination)
             {
-                var originalConstructor = _originalConstructorSelectorPolicy.SelectConstructor(context, resolverPolicyDestination);
-
-                if (originalConstructor.Constructor.GetParameters().All(arg => _container.CanResolve(arg.ParameterType)))
+                try
                 {
-                    return originalConstructor;
-                }
-                else
-                {
-                    var newSelectedConstructor = FindNewCtor(originalConstructor);
-                    if (newSelectedConstructor == null)
-                        return originalConstructor;
+                    var name = context.BuildKey.Type.Name;
+                    var originalConstructor = _originalConstructorSelectorPolicy.SelectConstructor(context, resolverPolicyDestination);
 
-                    foreach (var newParameterResolver in
-                        originalConstructor.GetParameterResolvers().Take(newSelectedConstructor.Constructor.GetParameters().Length))
+
+                    if (originalConstructor.Constructor.GetParameters().All(arg => _container.CanResolve(arg.ParameterType)))
                     {
-                        newSelectedConstructor.AddParameterResolver(newParameterResolver);
+                        return originalConstructor;
+                    }
+                    else
+                    {
+                        var newSelectedConstructor = FindNewCtor(originalConstructor);
+                        if (newSelectedConstructor == null)
+                            return originalConstructor;
+
+                        foreach (var parameter in newSelectedConstructor.Constructor.GetParameters())
+                        {
+                            newSelectedConstructor.AddParameterResolver(parameterResolver.GetResolver(parameter));
+                        }
+                        //    foreach (var newParameterResolver in
+                        //    originalConstructor.GetParameterResolvers().Take(newSelectedConstructor.Constructor.GetParameters().Length))
+                        //{
+                        //    newSelectedConstructor.AddParameterResolver(newParameterResolver);
+                        //}
+
+                        return newSelectedConstructor;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    var a = context.BuildKey.Type;
+
+                    var b = GetConstructorForType(a);
+                    foreach (var parameter in b.Constructor.GetParameters())
+                    {
+                        b.AddParameterResolver(parameterResolver.GetResolver(parameter));
                     }
 
-                    return newSelectedConstructor;
+                    return b;
+                    throw;
                 }
             }
+
+            /// <summary>
+            /// Exposes part of Unity's default policy which is marked protected.
+            /// </summary>
+            class ParameterResolver : DefaultUnityConstructorSelectorPolicy
+            {
+                public IDependencyResolverPolicy GetResolver(ParameterInfo parameterInfo)
+                {
+                    return CreateResolver(parameterInfo);
+                }
+            }
+
+            readonly ParameterResolver parameterResolver = new ParameterResolver();
 
             private SelectedConstructor FindNewCtor(SelectedConstructor originalConstructor)
             {
                 var implementingType = originalConstructor.Constructor.DeclaringType;
+                return GetConstructorForType(implementingType, originalConstructor);
+            }
+
+            private SelectedConstructor GetConstructorForType(Type implementingType, SelectedConstructor originalConstructor = null)
+            {
                 var constructors = implementingType.GetTypeInfo()
                   .DeclaredConstructors
-                  .Where(constructor => constructor.IsPublic && constructor != originalConstructor.Constructor)
+                  .Where(constructor => constructor.IsPublic && constructor != originalConstructor?.Constructor)
                   .ToArray();
                 if (constructors.Length == 0) return null;
 
@@ -86,9 +127,6 @@ namespace SInnovations.Unity.AspNetCore
 
                 return new SelectedConstructor(newCtor);
             }
-
-
-
         }
         public override void PreBuildUp(IBuilderContext context)
         {
